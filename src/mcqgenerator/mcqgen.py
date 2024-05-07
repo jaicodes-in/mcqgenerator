76f84b58575b7f9c60afb7a8107f7083c5c6e827
Jai Panchal <jaipanchal97@gmail.com>
2024-05-07 12:17:50 +0530

    updated

--- src/mcqgenerator/mcqgen.py
+++ src/mcqgenerator/mcqgen.py
@@ -35,14 +35,14 @@ Ensure to make {number} MCQs\
 
 quiz_generation_prompt=PromptTemplate(input_variables=['text','number','subject','tone','response_json'],template=TEMPLATE)
 
-quiz_chain=LLMChain(llm=llm_groq,prompts=quiz_generation_prompt,output_key='quiz',verbose='True')
+quiz_chain=LLMChain(llm=llm_groq,prompt=quiz_generation_prompt,output_key='quiz',verbose=True)
 
 TEMPLATE2="""You are an expert english grammarian and writer. Given a Multiple Choice Quiz for {subject} students.\
 You need to evaluate the complexity of the question and give a complete analysis of the quiz. \
 Only use at max 50 words for complexity analysis. \
 if the quiz is not at per with the cognitive and analytical abilities of the students,\
 update the quiz questions which needs to be changed and change the tone such that it perfectly fits the student abilities\
-Quiz_MCQs:
+Quiz_MCQs:\
 {quiz}
 
 Check from an expert English Writer of the above quiz:
